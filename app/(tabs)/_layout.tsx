4d00fe0a4b07589fe61ca63526379e2b9025d391
Bhuiyash Kumar <bhuiyashkr@gmail.com>
2026-01-01 01:38:34 +0530

    Toggle tab bar background color for improved visibility

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -47,8 +47,8 @@ export default function TabLayout() {
         tabBarActiveTintColor: '#3b82f6',
         tabBarInactiveTintColor: '#9ca3af',
         tabBarStyle: {
-          // backgroundColor: '#ffffff',
-          backgroundColor: '#0a0016',
+          backgroundColor: '#ffffff',
+          // backgroundColor: '#0a0016',
           borderTopWidth: 1,
           borderTopColor: '#e5e7eb',
           paddingBottom: 5,
